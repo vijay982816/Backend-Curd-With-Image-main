0fa4989f47a734dd2fb742c5df836fc2c78d1b34
hiteshbiet <hiteshkumarbiet@gmail.com>
2022-09-19 23:07:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+var cors = require('cors')
+const connectToMongo = require('./Database/db.js')
+
+
+//for using .env file or variables
+require('dotenv').config()
+
+
+// creating a server 
+
+const app = express()
+
+
+//bypassing  cors
+app.use(cors())
+
+//for parsing the incoming requests with JSON payloads
+
+app.use(express.json())
+
+//connecting to Db[MongoDb]
+connectToMongo()
+
+
+// Available Routes
+app.use('/api/users/', require('./Routes/User.js'));
+
+
+
+
+
+// configure the server's port 
+
+const port = process.env.PORT || 3000;
+
+//listening server on a port 
+
+app.listen(port, err => {
+    if (err)
+        throw err
+    console.log('Server listening on port', port)
+})
